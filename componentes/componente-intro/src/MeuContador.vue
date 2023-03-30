1c409db68e351471cacca707735d69c913417b09
alofrrr <analauraoferreira1@gmail.com>
2023-03-30 01:23:15 -0300

    feat: componentes

---
+++ componentes/componente-intro/src/MeuContador.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="contador">
+        <h1>Contador</h1>
+        <span>{{ contador }}</span>
+        <button @click="adicionar">Adicionar</button>
+        <button @click="subtrair">Subtrair</button>
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            contador: 0
+        }
+    },
+    methods: {
+        adicionar(){
+            this.contador++
+        },
+        subtrair(){
+            this.contador--
+        }
+    }
+    
+}
+</script>
+
+<style>
+.contador span {
+    border-bottom: 1px solid #ccc;
+    height: 30px;
+    padding: 5px 25px;
+}
+
+.contador button {
+    background-color: coral;
+    height: 30px;
+    width: 30px;
+    border-radius: 15px;
+    color: #fff;
+    margin-left: 20px;
+    outline: none;
+}
+
+</style>
---
+++ componentes/componente-intro/src/MeusContadores.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div class="contadores">
+    <app-contador v-for="c in 5" :key="c"/>
+  </div>
+</template>
+
+<script>
+import MeuContador from './MeuContador.vue'
+
+export default {
+    components: {
+        'app-contador': MeuContador
+    }
+
+}
+</script>
+
+<style>
+
+</style>
